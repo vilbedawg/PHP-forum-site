12e13607261de0b11e5f39d1de156f040a68357b
Vilho Luoma <vilho.luoma@gmail.com>
2021-12-02 10:59:05 +0200

    commit

--- users.php
+++ users.php
@@ -64,23 +64,7 @@ include_once "header.php";
 
 
     </body>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/socket.io/1.7.4/socket.io.min.js'></script>
     <script>
-        var socket = io.connect('https://site.test:436');
-        console.log(socket);
-
-        socket.on('connect', function () {
-            console.log('connected');
-
-            socket.on('broadcast', function (data) {
-                //console.log(data);
-                //socket.emit("broadcast", data);
-                alert(data.text);
-            });
-
-            socket.on('disconnect', function () {
-                console.log('disconnected');
-            });
-        }); 
+       
     </script>
     </html>
